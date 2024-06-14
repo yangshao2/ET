8dcfb546f87964decb6aa429162ed8382eceb122
yshao <yshao@vt.edu>
2024-06-14 09:34:26 -0400

    added notes

---
+++ Readme.txt
@@ -0,0 +1,11 @@
+Step1: resample all ET data using R_resample.R 
+Results will be saved in a folder named as resampled_files
+
+Step2: Download et_ref from Gridmet 
+https://www.northwestknowledge.net/metdata/data/
+
+Step3: extract daily ET from nc files 
+extract_dailyetr_from_gridmet.R
+
+Step4: calculate et ratio or fraction (eta/etref)
+use 
--- generate_etratio.R
+++ generate_etratio.R
@@ -1,21 +1,30 @@
-library(raster)
+library(terra)
 library(rgdal)
+#all et_ref images (tifs) should be in the folder
+etr_path<-"C:/lydia/gridmet/"
 
-setwd("~/work/lydia")
+#setwd 
+#setwd("C:/etdata1435/resampled_files")
+filelist<-Sys.glob('*.tif')
 
-filelist<-list.files('./eeflux',pattern='.tif')
+# Create an output folder for resampled files
+outputFolder <- "../etratio"
+if (!dir.exists(outputFolder)) {
+  dir.create(outputFolder)
+}
+
 
 for (i in 1:length(filelist)){
-  eeflux_file<-paste0('./eeflux/',filelist[i])
-  eta<-raster(eeflux_file)
-  #create a pattern (partical file name)
-  namepattern<-paste0('./etr_gridmet/p*',substr(filelist[i],12,15),"_",substr(filelist[i],16,18),'*')
-  #list etr file matching the file name pattern
-  etr_file<-Sys.glob(namepattern)
-  etr<-raster(etr_file)
-  #compute ratio of eta and etr
-  et_ratio<-eta/etr
-  outfile<-paste0('./et_ratio/etratio',substr(filelist[i],12,15),'_',substr(filelist[i],16,18),'.tif')
+doy<-substr(filelist[i],14,16)
+year<-substr(filelist[i],10,13)
+
+a<-rast(filelist[1])
+
+et_ref<-paste0(etr_path,"etr_",year,"_",doy,".tif")
+r<-rast(et_ref)
+r_p <- project(r, a)
+et_ratio <- a/r_p
+
+outfile<-paste0(outputFolder,'/etratio_',year,'_',doy,'.tif')
 writeRaster(et_ratio,outfile,overwrite=T)
-  }
-  
+}
